ffa89ab70a24cab52247c7c0367412311e6375ad
Jeff Xiong <gigix1980@gmail.com>
2011-07-10 21:20:23 +0800

    error handling in fetching cron task

--- rails_root/script/fetch.rb
+++ rails_root/script/fetch.rb
@@ -1,11 +1,11 @@
 User.find(:all).each do |user|
-  begin 
     user.feeds.each do |feed|
-      feed.fetch!
+      begin 
+        feed.fetch!
+      rescue => e
+        puts "Error happens when fetching feed ##{feed.id} of user ##{user.id} :"
+        puts e.message
+        puts e.backtrace.join("\n")
+      end
     end
-  rescue => e
-    puts "Error happens when fetching feed ##{feed.id} of user ##{user.id} :"
-    puts e.message
-    puts e.backtrace.join("\n")
-  end
 end
