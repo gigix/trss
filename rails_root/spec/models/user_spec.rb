c9f3c48a3b60a46616c3a6a8c6b6ed1c769b89cd
Jeff Xiong <gigix1980@gmail.com>
2011-07-17 11:22:47 +0800

    defect fix

--- rails_root/spec/models/user_spec.rb
+++ rails_root/spec/models/user_spec.rb
@@ -10,6 +10,9 @@ describe User do
   describe :sync! do
     before(:each) do
       @weibo = SinaWeiboToken.new
+      @access_token = mock(:access_token)
+      @weibo.stub!(:client).and_return(@access_token)
+      
       @user = create_test_user
       @user.sina_weibo = @weibo
     end
@@ -17,7 +20,7 @@ describe User do
     it 'syncs feed items to micro blog' do
       feed = @user.feeds.create!
       feed.feed_items.create!(:title => "something", :link => "http://some.site/path")
-      @weibo.should_receive(:post).with("http://api.t.sina.com.cn/statuses/update.json", {:status => "something <http://some.site/path>"})
+      @access_token.should_receive(:post).with("http://api.t.sina.com.cn/statuses/update.json", {:status => "something <http://some.site/path>"})
       
       @user.sync!
     end
@@ -25,7 +28,7 @@ describe User do
     it 'does not sync feed items which were synced already' do
       feed = @user.feeds.create!
       feed.feed_items.create!(:title => "something", :link => "http://some.site/path")
-      @weibo.should_receive(:post).with("http://api.t.sina.com.cn/statuses/update.json", {:status => "something <http://some.site/path>"})
+      @access_token.should_receive(:post).with("http://api.t.sina.com.cn/statuses/update.json", {:status => "something <http://some.site/path>"})
       
       @user.sync!
       @user.sync!
