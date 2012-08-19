81915a52d1884b2e37c6e2969794ba8448f53c5d
Jeff Xiong <gigix1980@gmail.com>
2012-08-19 09:51:49 -0400

    use new sina api

--- rails_root/app/models/sina_weibo_token.rb
+++ rails_root/app/models/sina_weibo_token.rb
@@ -1,5 +1,5 @@
 class SinaWeiboToken < ConsumerToken
   def add_status!(status)
-    client.post("http://api.t.sina.com.cn/statuses/update.json", {:status => status})
+    client.post("http://api.weibo.com/2/statuses/update.json", {:status => status})
   end
 end
